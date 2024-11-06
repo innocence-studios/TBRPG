c5b7b51af7d1e2bfef59536cee244cbbf813c054
Reidelrick <alexis.leviavant@gmail.com>
2024-11-06 10:44:57 +0100

    Initial commit

---
+++ Enemies/Resources/Enemy.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name EnemyResource
+
+@export var name : String
+@export var hp : int
+@export var damage : int
+@export var sprite : String ##Sprite name under res://Enemies/Sprites
---
+++ Scenes/Resources/EN_Combat.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name CombatEncounter
+
+@export var enemies:Array[EnemyResource]
