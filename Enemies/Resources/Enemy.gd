e23eae4ddada37b1a89b9cb9d5592f0c8d736e27
Reidelrick <alexis.leviavant@gmail.com>
2024-11-23 18:03:20 +0100

    things

--- Enemies/Resources/Enemy.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name EnemyResource
-
-@export var name : String
-@export var hp : int
-@export var damage : int
-@export var sprite : String ##Sprite name under res://Enemies/Sprites
--- Scenes/EN_Combat.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-class_name CombatEncounter
-
-@export var enemies:Array[EnemyResource]
